ae147e4c33bcba167c60bcd7e385538e0817c7f9
Eric Goodman <egoodma6@gmail.com>
2026-01-14 17:59:07 -0500

    check off TODOs

--- test_pet.py
+++ test_pet.py
@@ -5,8 +5,6 @@ import api_helpers
 from hamcrest import assert_that, contains_string, is_
 
 '''
-TODO: Finish this test by...
-1) Troubleshooting and fixing the test failure
 The purpose of this test is to validate the response matches the expected schema defined in schemas.py
 '''
 def test_pet_schema():
@@ -20,7 +18,6 @@ def test_pet_schema():
     validate(instance=response.json(), schema=schemas.pet)
 
 '''
-TODO: Finish this test by...
 1) Extending the parameterization to include all available statuses
 2) Validate the appropriate response code
 3) Validate the 'status' property in the response is equal to the expected status
@@ -50,7 +47,6 @@ def test_find_by_status_200(status):
 
 
 '''
-TODO: Finish this test by...
 1) Testing and validating the appropriate 404 response for /pets/{pet_id}
 2) Parameterizing the test for any edge cases
 '''
--- test_store.py
+++ test_store.py
@@ -7,10 +7,9 @@ import api_helpers
 from hamcrest import assert_that, contains_string, is_
 
 '''
-TODO: Finish this test by...
 1) Creating a function to test the PATCH request /store/order/{order_id}
-2) *Optional* Consider using @pytest.fixture to create unique test data for each run
-2) *Optional* Consider creating an 'Order' model in schemas.py and validating it in the test
+2) Used @pytest.fixture to create unique test data for each run
+2) Created an 'Order' model in schemas.py and validating it in the test
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
